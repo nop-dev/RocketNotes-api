690c01ab20a93f2681369893df77bf5a2dccb7fd
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-04-16 17:34:11 -0300

    feat: :construction: ultilizando query params

--- src/server.js
+++ src/server.js
@@ -2,14 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/message/:id/:user", (request, response) => {
+app.get("/message/:id/:user", (request, response) => { // aqui os parâmetros são obrigatórios
     const { id, user } = request.params;
     // response.send("Hello World!")
 
     response.send(`
-    ID da mensagem ${id}
+    ID da mensagem ${id};
     para o usuário ${user}`);
 });
+
+app.get("/users", (request, response) => { // aqui os parâmetros não são obrigatórios
+    const { page, limit } = request.query;
+
+    response.send(`Essa é a page ${page} com limite ${limit}`);
+}); 
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
