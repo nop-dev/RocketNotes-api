8f94786e5e3ac070220957ea2be578755d6b15bf
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-04-16 17:16:29 -0300

    feat: :sparkles: adiciona código relativo ao uso de parâmetros

--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send("Hello World!")
-})
+app.get("/message/:id/:user", (request, response) => {
+    const { id, user } = request.params;
+    // response.send("Hello World!")
+
+    response.send(`
+    ID da mensagem ${id}
+    para o usuário ${user}`);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
