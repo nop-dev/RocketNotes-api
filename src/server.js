309f0c8a41ace32b020957a093388a617668ed7a
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-04-20 11:57:08 -0300

    feat: :truck: reorganiza arquivos, cria o /routes

--- src/server.js
+++ src/server.js
@@ -1,21 +1,11 @@
 const express = require("express");
 
+const routes = require("./routes")
+
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => { // aqui os parâmetros são obrigatórios
-    const { id, user } = request.params;
-    // response.send("Hello World!")
-
-    response.send(`
-    ID da mensagem ${id};
-    para o usuário ${user}`);
-});
-
-app.get("/users", (request, response) => { // aqui os parâmetros não são obrigatórios
-    const { page, limit } = request.query;
-
-    response.send(`Essa é a page ${page} com limite ${limit}`);
-}); 
+app.use(routes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
