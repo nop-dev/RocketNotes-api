82c69a6a24d844d54d560d41f8910635164ec130
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-04-23 17:48:13 -0300

    feat: :construction: adiciona migration para automatizar a criação do banco de dados

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('express-async-errors');
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require('./utils/AppError');
 
 const express = require("express");
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
-database();
+migrationsRun();
 
 app.use(( error, request, response, next ) => {
  if(error instanceof AppError) {
