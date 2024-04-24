69d305c48863ee63ed8c45de63a817b4e16d6f25
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-04-24 14:50:18 -0300

    feat: :construction: adiciona função para a pessoa não cadastrar mais de uma vez o mesmo e-mail

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,23 @@
 const AppError = require("../utils/AppError")
 
+const sqliteConnection = require("../database/sqlite")
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
 
         if(!name) {
             throw new AppError("O nome é obrigatório...");
         }
 
+        const database = await sqliteConnection();
+        const checkIfUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+        if(checkIfUserExists) {
+            throw new AppError("Este e-mail já está em uso...")
+        };
+        
         response.status(201).json({ name, email, password});
+
+        return response.status(201).json();
     }
 };
 
