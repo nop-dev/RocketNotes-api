8e2cead29382de94110ed61ef68133ab7cc796a1
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-04-24 15:36:21 -0300

    feat: :sparkles: adiciona método que atualiza nome e email do user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 
-const UsersController = require("../controllers/UsersController")
+const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
@@ -8,14 +8,15 @@ function myMiddleware(request, response, next) {
     console.log("Você passou pelo MiddleWare...");
 
     if(!request.body.isAdmin) {
-        return response.json( { message: "usuário não autorizado" } )
-    }
+        return response.json( { message: "usuário não autorizado" } );
+    };
 
     next();
 }
 
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create)
+usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
